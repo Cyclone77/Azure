966d574ad4cd2c800fa583bfac1c85da6a6b275c
cyclone77 <fillchar@live.com>
2016-12-02 23:35:03 +0800

    维护

--- js/Default.js
+++ js/Default.js
@@ -14,17 +14,14 @@ function showTag(tagStr) {
         $('#show-tag').empty(content);
         var content = "<h2>分类：" + tagStr + "</h2><ul class=\"posts\">";
         var count = 0;
-        $.each(data,
-            function(i, item) {
-                $.each(item.tags,
-                    function(j, tag) {
-                        if (tag == tagStr) {
-                            content += "<li class=\"listing-item\"><time datetime=\"" + item.date + "\">" + item.date + "</time><a href=\"" + item.url + "\">" + item.title + "</a></li>";
-                            count++;
-                        }
-
-                    });
+        $.each(data, function(i, item) {
+            $.each(item.tags, function(j, tag) {
+                if (tag == tagStr) {
+                    content += "<li class=\"listing-item\"><time datetime=\"" + item.date + "\">" + item.date + "</time><a href=\"" + item.url + "\">" + item.title + "</a></li>";
+                    count++;
+                }
             });
+        });
         if (count > 0) {
             content += "</ul>";
             postNumStr = "<span>（" + count + "篇文章）</span>";
